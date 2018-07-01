38e27b18980c226f738734ded6d60c17481d3a47
kiraheta <iraheta.kenny@gmail.com>
2018-07-01 00:12:51 -0700

    fix cannot GET

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
-var path = require('path'); 
-var HtmlWebpackPlugin = require('html-webpack-plugin');  
-// creates an index html file inside dist folder incl index_bundle.js file 
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+// creates an index html file inside dist folder incl index_bundle.js file
 
 module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+
   },
   module: {
     rules: [  // loaders
       // babel-loader will be used on all files w/ ext .js
-      { test: /\.(js)$/, use: 'babel-loader' }, 
+      { test: /\.(js)$/, use: 'babel-loader' },
       // these loaders will be used on files w/ ext .css
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]} 
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
      ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
