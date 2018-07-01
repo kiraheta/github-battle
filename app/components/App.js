81b4a8fc915d885f7c5b8335f605735955cb5696
kiraheta <iraheta.kenny@gmail.com>
2018-07-01 00:25:01 -0700

    refactor routes into switch

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,7 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
@@ -13,9 +14,14 @@ class App extends React.Component {
       <Router>
         <div className="container">
           <Nav />
-          <Route exact path='/' component={Home} />
-          <Route path='/popular' component={Popular} />
-          <Route path='/battle' component={Battle} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/popular' component={Popular} />
+            <Route exact path='/battle' component={Battle} />
+            <Route render={function() {
+              return <p> Not found! </p>
+            }}/>
+          </Switch>
         </div>
       </Router>
     )
