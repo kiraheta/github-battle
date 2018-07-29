8fdfeed82bf2ed7184478299398f078c44654bf3
kiraheta <iraheta.kenny@gmail.com>
2018-07-28 17:50:13 -0700

    add state management in results component

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,11 +4,64 @@ var api = require('../utils/api');
 var Link = require('react-router-dom').Link;
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+
+  componentDidMount () {
+    var players = queryString.parse(this.props.location.search);
+
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function (results) {
+      if (results === null) {
+        return this.setState(function () {
+          return {
+            error: 'Looks like there was error. Check that both users exist on Github',
+            loading: false,
+          }
+        });
+      }
+
+      this.setState(function () {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      });
+    }.bind(this));
+  }
+
   render() {
-    console.log(this.props)
+    var error = this.state.error;
+    var winner = this.state.winner;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      <p>Loading</p>
+    }
+
+    if (error) {
+      <div>
+        <p>{error}</p>
+        <Link to='/battle'>Reset</Link>
+      </div>
+    }
+
     return (
       <div>
-        Results
+        {JSON.stringify(this.state, null, 2)}
       </div>
     )
   }
